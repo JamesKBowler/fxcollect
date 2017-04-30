281e37ec2c351a3477b7a178a2eb5212654e0db7
JamesKBowler <jbowler1983@gmail.com>
2017-04-30 08:11:11 +0100

    Update settings.py

--- fxcmminer_v1.0/settings.py
+++ fxcmminer_v1.0/settings.py
@@ -6,7 +6,7 @@ FX_ENVR = 'real' # or demo, data is different on the demo account!!
 # MariaDB Credentials
 DB_USER = 'sec_master'
 DB_PASS = 'password'
-DB_HOST= 'localhost'
+DB_HOST = 'localhost'
 XML_FILE = 'catalog.xml'
 LOGDIR = 'logs/'
 TIME_FRAMES = ['M1','W1','D1','H8', 'H4', 'H2', 'H1','m30', 'm15', 'm5', 'm1']
