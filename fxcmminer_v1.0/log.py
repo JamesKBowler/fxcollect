dd0a1b8ab573b23c3c382e47c1aa2248070bc4d0
James Bowler <jbowler1983@gmail.com>
2017-05-02 16:37:22 -0400

    Update log.py

--- fxcmminer_v1.0/log.py
+++ fxcmminer_v1.0/log.py
@@ -1,10 +1,14 @@
 import logging
 from logging import DEBUG
+from settings import LOG_DIR
+import re
 
 class Logger(object):
     def __init__(self, filename):
-        self.filename = 'logs/%s.log' % filename
-        logging.basicConfig(filename=self.filename,level=DEBUG)
+        
+        fn = '%s/%s.log' % (LOG_DIR, re.sub('[^A-Za-z0-9]+','',filename))
+        
+        logging.basicConfig(filename=fn,level=DEBUG)
 
     def debug(self, message):
         logging.debug(message)
