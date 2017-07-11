06277ea30094ff6669537f2365b6ad9f5a19642b
James Bowler <jbowler1983@gmail.com>
2017-07-11 21:44:02 +0100

    Update laundry.py

--- fxcmminer_v1.1/fxcmminer/cleaning/laundry.py
+++ fxcmminer_v1.1/fxcmminer/cleaning/laundry.py
@@ -2,24 +2,24 @@ from event import CleanedDataEvent
 
 class DataCleaner(object):
     """
-    Basic data cleaning
+    The DataCleaner class is the process of correcting
+    (or removing) corrupt or inaccurate records from a record set
+    and refers to identifying incomplete, incorrect, inaccurate
+    or irrelevant parts of the data and then replacing,
+    modifying, or deleting the dirty or coarse data.
+    Most of the above is not implemented in the code below.
     """
     def __init__(self, events_queue):
-        """
-        """
+        """ Initialize varables """
         self.events_queue = events_queue
 
     def _remove_duplicates(self, data):
-        """
-        Drop any duplicates in the Datetime Index
-        """
+        """ Drop any duplicates in the Datetime Index """
         return data.reset_index().drop_duplicates(
             subset='date', keep='last').set_index('date')
 
     def _remove_not_a_number(self, data):
-        """
-        Drop any rows that contain NaN values.
-        """
+        """ Drop any rows that contain NaN values """
         return data.dropna()
 
     def _remove_incorrect_values(
@@ -27,9 +27,7 @@ class DataCleaner(object):
         ac='askclose', bo='bidopen',bh='bidhigh', bl='bidlow',
         bc='bidclose', v='volume'
     ):
-        """
-        Removes errors from the open high low close values.
-        """
+        """ Removes errors from the open high low close values """
         data = data.loc[data[ac] <= data[ah]]
         data = data.loc[data[ac] >= data[al]]
         data = data.loc[data[ao] <= data[ah]]
@@ -44,6 +42,7 @@ class DataCleaner(object):
         return data
 
     def clean_data(self, event):
+        """ Encapsulates the above cleaning processes """
         data = self._remove_not_a_number(event.data)
         data = self._remove_incorrect_values(data)
         data = self._remove_duplicates(data)
