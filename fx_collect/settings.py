e8c1a5e45c7cfa85235057e93b856dbbaf6d3518
JamesKBowler <jbowler1983@gmail.com>
2018-01-17 21:04:21 +0000

    adding new code

---
+++ fx_collect/settings.py
@@ -0,0 +1,10 @@
+# FXCM Account Credentials
+FX_USER = ''
+FX_PASS = ''
+FX_ENVR = 'real'
+URL = "http://www.fxcorporate.com/Hosts.jsp"
+
+# MariaDB Credentials
+DB_USER = 'sec_master'
+DB_PASS = ''
+DB_HOST= 'localhost'
