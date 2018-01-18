ab9ae698d454e3fd4f09b678852fbf3cb410fe8d
James Bowler <jbowler1983@gmail.com>
2018-01-18 16:57:02 +0000

    Update instrument.py

--- fx_collect/instrument.py
+++ fx_collect/instrument.py
@@ -5,58 +5,57 @@ class Instrument(object):
         self, broker, instrument, time_frame,
         market_status, last_update, dt
     ):
-        trading_wk_str = dt - timedelta(days=dt.weekday()+1)
-        self.sw_hour = trading_wk_str.hour
-        self.td = trading_wk_str.hour - 22
+        # Start of Trading Week
+        self.trading_wk_str = dt - timedelta(days=dt.weekday()+1)
+        self.sw_hour = self.trading_wk_str.hour
+        self.td = self.trading_wk_str.hour - 22
         # Passport
         self.instrument = instrument
         self.time_frame = time_frame
         self.market_status = market_status
         self.last_update = last_update
+        # Dates
         self.db_min = None
         self.db_max = None
         self.fin_bar = None
 
-    def update(
-        self, lastupdate, market_status
-    ):
+    def update(self, lastupdate, market_status):
         self.last_update = lastupdate
         self.market_status = market_status
       
     def calculate_finished_bar(self):
         """
-        Create time delta resolution for finding the 
-        last finished bar published by FXCM. This is to stop
-        unfinished bars being written to the database.
+        Stops unfinished bars from being written to the
+        database by calculating the latest finshed bar.
         """
         lu = self.last_update.replace(second=0,microsecond=0)
         tf = int(self.time_frame[1:])
         
-        # New Your Offset
+        # New York Offset
         if self.td % 2 == 0: nyo = 1
         else: nyo = 2
           
         if self.time_frame[:1] == "m":  # Minute
-            ub = lu
+            adj = lu
             l = list(range(0,60,tf))
-            cm = min(l, key=lambda x:abs(x-ub.minute))
-            fin = ub.replace(minute=cm)-timedelta(minutes=tf)
+            cm = min(l, key=lambda x:abs(x-adj.minute))
+            fin = adj.replace(minute=cm)-timedelta(minutes=tf)
             
         elif self.time_frame[:1] == "H":  # Hour
-            ub = lu.replace(minute=0)
-            h = ub.hour
+            adj = lu.replace(minute=0)
             l = [e-nyo for e in [i + tf - 2 for i in list(range(1,25,tf))]]
-            ch = min(l, key=lambda x:abs(x-h))
-            fin = ub.replace(hour=ch)-timedelta(hours=tf)            
+            ch = min(l, key=lambda x:abs(x-adj.hour))
+            fin = adj.replace(hour=ch)-timedelta(hours=tf)            
 
         elif self.time_frame[:1] == "D":  # Day
-            init_bar = lu.replace(hour=self.sw_hour,minute=0)
-            fin = init_bar - timedelta(days=tf)
+            adj = lu.replace(hour=self.sw_hour,minute=0)
+            fin = adj - timedelta(days=tf)
             
         elif self.time_frame[:1] == "M":  # Month
-            init_bar = lu.replace(day=1,hour=self.sw_hour,minute=0)
-            fin = init_bar - timedelta(days=1)
+            adj = lu.replace(day=1,hour=self.sw_hour,minute=0)
+            fin = adj - timedelta(days=1)
+
         else:
-            raise NotImplmented("Time-frame : %s Not Supported" % self.time_frame)
+            raise NotImplmented("Time-frame : %s Not Supported")
             
         self.fin_bar = fin
