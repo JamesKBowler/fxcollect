8aa579b82ea77a5fa0444637292345582ca1c92d
James Bowler <jbowler1983@gmail.com>
2018-01-20 17:21:12 +0000

    Update instrument.py

--- fx_collect/instrument.py
+++ fx_collect/instrument.py
@@ -1,39 +1,60 @@
 class InstrumentAttributes(object):
     def __init__(
         self, broker, instrument, time_frames,
-        market_status, last_update, utc_now, wk_str, wk_end
+        market_status, last_update, utc_now,
+        wk_str, wk_end
     ):
+        # Passport
+        self.instrument = instrument
+        self.market_status = market_status
+        self.last_update = last_update
+        self.time_frames = time_frames
         # Start of Trading Week
         self.utc_now = utc_now
         self.wk_str = wk_str
         self.wk_end = wk_end
         self.str_hour = wk_str.hour
         self.td = wk_str.hour - 22
-        # Passport
-        self.instrument = instrument
-        self.market_status = market_status
-        self.last_update = last_update
-        self.time_frames = time_frames
+        # Ticks
+        self.bid = None
+        self.ask = None
         # Time frame storage dict
-        self.attrib = {}
+        self.attribs = {}
         for time_frame in time_frames:
-            self.attrib[time_frame] = {
-              'db_min' : None,
-              'db_max' : None,
-              'finbar' : None
+            self.attribs[time_frame] = {
+                'db_min' : None,
+                'db_max' : None,
+                'finbar' : None
             }
 
     def update_instrument_status(
-        self, lastupdate, market_status, utc_now
+        self, lastupdate, market_status,
+        utc_now, bid, ask
     ):
         self.utc_now = utc_now
         self.last_update = lastupdate
         self.market_status = market_status
+        self.bid = bid
+        self.ask = ask
 
     def update_database_datetime(
         self, time_frame, pdfm, pdto
     ):
-        if pdfm < self.attrib[time_frame]['db_min']: 
-            self.attrib[time_frame]['db_min'] = pdfm
-        if pdto >= self.attrib[time_frame]['db_max']:
-            self.attrib[time_frame]['db_max'] = pdto
+        if pdfm < self.ttribs[time_frame]['db_min']: 
+            self.attribs[time_frame]['db_min'] = pdfm
+        if pdto >= self.attribs[time_frame]['db_max']:
+            self.attribs[time_frame]['db_max'] = pdto
+            
+    def create_snapshot(self):
+        return {
+            self.instrument : {
+                 'utc_now' : self.utc_now,
+                 'wk_str' : self.wk_str,
+                 'wk_end' : self.wk_end,
+                 'market_status' : self.market_status,
+                 'last_update' : self.last_update,
+                 'bid' : self.bid,
+                 'ask' : self.ask,
+                 'time_frames' : self.attribs
+            }
+        }
