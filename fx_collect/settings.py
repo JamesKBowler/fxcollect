ea24becc8651538339e692d59ceb8417a77d3d7b
James Bowler <jbowler1983@gmail.com>
2018-01-21 10:54:18 +0000

    Update settings.py

--- fx_collect/settings.py
+++ fx_collect/settings.py
@@ -1,9 +1,11 @@
+import os
+
 # FXCM Account Credentials
 FX_USER = ''
 FX_PASS = ''
 FX_ENVR = 'real'
 URL = "http://www.fxcorporate.com/Hosts.jsp"
-
+JSON_DIR = os.getcwd() + '/json_files/'
 # MariaDB Credentials
 DB_USER = 'sec_master'
 DB_PASS = ''
