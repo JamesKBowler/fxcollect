4cbf5684c5e284995f500cb026aa6221b59a5ea8
James Bowler <jbowler1983@gmail.com>
2018-03-01 08:00:37 +0000

    removing unused method

--- fx_collect/utils/date_utils.py
+++ fx_collect/utils/date_utils.py
@@ -51,25 +51,6 @@ def fm_string(dt_string, millisecond=False):
         return datetime.strptime(
             dt_string, '%Y/%m/%d %H:%M:%S')
 
-def switch_day(
-    dt, day=6, hour=22, minute=0,
-    second=0, microsecond=0,
-    add_week=False, sub_week=False
-):
-    """
-    Move date to next specified day at 22:00UTC
-    """
-    day_num = dt.weekday()
-    days_until = (7 - day_num + day) % 7
-    if add_week:
-        days_until+=7
-    if sub_week:
-        days_until-=7
-    _sunday = dt + timedelta(days=days_until)
-    return _sunday.replace(
-        hour=hour, minute=minute,
-        second=second, microsecond=microsecond)
-
 def new_york_offset(dt):
     """
     Adjusts the time based on the US/Eastern timezone,
